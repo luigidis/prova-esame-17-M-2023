f98c5f6d6e17804b1062b73e75971ad462d40589
luigi.distefano <luigi.distefano69@gmail.com>
2025-02-05 01:52:34 +0100

    point A e B

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct parameters {
+typedef struct parameters
+{
     int a;
     int b;
     float alpha;
@@ -9,17 +10,18 @@ typedef struct parameters {
 } Parameters;
 
 Parameters readInput(int, char **);
- 
+double **allocMatrix(int, int);
+
 int main(int argc, char *argv[])
 {
 
     printf("=================POINT A=================\n");
-    Parameters p  = readInput(argc, argv);
+    Parameters p = readInput(argc, argv);
     printf("=================POINT B=================\n");
+    double **matrix = allocMatrix(p.a, p.b);
     printf("=================POINT C=================\n");
     printf("=================POINT D=================\n");
     printf("=================POINT E=================\n");
-
 }
 
 void error(char *error)
@@ -28,7 +30,7 @@ void error(char *error)
     exit(1);
 }
 
-Parameters readInput(int argc,char *argv[])
+Parameters readInput(int argc, char *argv[])
 {
     if (argc != 5)
         error("Wrong number of parameters");
@@ -41,15 +43,40 @@ Parameters readInput(int argc,char *argv[])
     if (b < 5 || b > 20)
         error("Worng parameters b");
 
-    float alpha  = atof(argv[3]);
+    float alpha = atof(argv[3]);
     if (alpha < 10 || alpha > 20)
         error("Worng parameters alpha");
 
     float beta = atof(argv[4]);
     if (beta < 1 || beta > 5)
         error("Worng parameters beta");
-    
+
     Parameters p = {a, b, alpha, beta};
 
     return p;
+}
+
+double **allocMatrix(int a, int b)
+{
+    // Alloco la memoria alla matrice di dimensioni A x B
+    double **M = (double **)malloc(sizeof(double *) * a);
+    if (M == NULL)
+        error("Allocation for **M not correctly done");
+
+    for (int i = 0; i < a; i++)
+    {
+        M[i] = (double *)malloc(sizeof(double) * b);
+        //Se l'allocazione non avviene correttamente
+        if (M[i] == NULL)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                free(M[j]);
+            }
+            free(M);
+            error("Memory allocation during M[i] not correctly done");
+        }
+    }
+
+    return  M;
 }
